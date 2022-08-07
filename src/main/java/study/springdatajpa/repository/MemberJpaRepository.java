51cb6145c6c58e22e42d7fe015214df3a8634229
xhaktmchl <xhaktmchl@naver.com>
2022-08-07 17:04:26 +0900

    feat: 멤버 기본 crud테스트 작성

--- src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.springdatajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {// JPA기반 리파지토리
@@ -18,8 +20,29 @@ public class MemberJpaRepository {// JPA기반 리파지토리
         return member;
     }
 
+    public void delete(Member member){
+        em.remove(member);
+    }
+
     // 회원 조회
     public Member find(Long id){
         return em.find(Member.class, id); // (찾는 클래스, PK)
     }
+
+    // 회원 조회
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id); // (찾는 클래스, PK)
+        return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+
 }
