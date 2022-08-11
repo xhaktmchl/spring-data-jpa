cff8a4f40896e2d5f6b649bc4fe1ffb8a2a5d512
xhaktmchl <xhaktmchl@naver.com>
2022-08-12 01:39:05 +0900

    feat: 페이징, 글로벌 설정, 페이징 개별 설정

---
+++ src/main/java/study/springdatajpa/controller/MemberController.java
@@ -0,0 +1,52 @@
+package study.springdatajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.springdatajpa.entity.Member;
+import study.springdatajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+
+    /*
+    도메인 컨버터 사용 전
+     */
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    /*
+    도메인 컨버터 사용 후
+     */
+//    @GetMapping("/members/{id}")
+//    public String findMember(@PathVariable("id") Member member) {
+//        return member.getUsername();
+//    }
+
+    /*
+    페이징과 정렬 예제
+     */
+    @GetMapping("/members") // /members?page=0&size=3&sort=id,desc&sort=username,desc
+    //@PageableDefault : 개별 페이징 설정
+    public Page<Member> list(@PageableDefault(size = 5,  sort = "username", direction =  Sort.Direction.DESC) Pageable pageable){
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+}
--- src/main/java/study/springdatajpa/entity/BaseTimeEntity.java
+++ src/main/java/study/springdatajpa/entity/BaseTimeEntity.java
@@ -1,9 +1,7 @@
 package study.springdatajpa.entity;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -15,19 +13,11 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseEntity extends BaseTimeEntity{ // 필요한 상황에 따라 분리해서 상속받아 쓴다.
-
+// 시간을 다로 BaseEntity로 두고 싶을 때 분리하고, BaseEntity에 상속
+public class BaseTimeEntity {
     @CreatedDate
-    @Column(updatable = false)// 수정 불가능
+    @Column(updatable = false)
     private LocalDateTime createdDate;
-
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
-
-//    @CreatedBy
-//    @Column(updatable = false)
-//    private String createdBy;
-//
-//    @LastModifiedBy
-//    private String lastModifiedBy;
 }
